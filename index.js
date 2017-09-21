ee11348dfaeff438a39ff629b7e6b524cb48e847
Nick Papoutsis <izybit@gmail.com>
2017-09-21 23:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function shout (string) {
+function shout(string) {
   return string.toUpperCase();
 }
+
+function wisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
